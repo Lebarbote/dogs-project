076db1606c8aa7b12b7175f2f68c95275b259cd5
Leticia Barbosa <leticiab.teixeira@yahoo.com.br>
2025-01-19 16:43:32 -0300

    adjustments and create userContext

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -13,8 +13,8 @@ const useForm = (type) => {
   const [error, setError] = React.useState(null);
 
   function validate(value) {
-    if (type == false) return true;
-    if (value.length == 0) {
+    if (type === false) return true;
+    if (value.length === 0) {
       setError('Preencha um valor.');
       return false;
     } else if (types[type] && !types[type].regex.test(value)) {
@@ -37,7 +37,7 @@ const useForm = (type) => {
     onChange,
     error,
     validate: () => validate(value),
-    onblur: () => validate(value),
+    onBlur: () => validate(value),
   };
 };
 
