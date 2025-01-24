f14e468385e0cf65639eef8a3aacf23a879dcb61
Leticia Barbosa <leticiab.teixeira@yahoo.com.br>
2025-01-24 14:49:08 -0300

    create login background

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,6 +5,7 @@ import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginCreate';
 import LoginPasswordReset from './LoginCreate';
 import { UserContext } from '../../UserContext';
+import styles from './Login.module.css';
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
@@ -12,14 +13,16 @@ const Login = () => {
   if (login === true) return <Navigate to="/account" />;
 
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="create" element={<LoginCreate />} />
-        <Route path="lost" element={<LoginPasswordLost />} />
-        <Route path="reset" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="create" element={<LoginCreate />} />
+          <Route path="lost" element={<LoginPasswordLost />} />
+          <Route path="reset" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
