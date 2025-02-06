7dc3b5c000ba9ed36f00a7c362cfc7bb797e5f1d
Leticia Barbosa <leticiab.teixeira@yahoo.com.br>
2025-02-06 16:37:23 -0300

    Create Photo Post

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -16,7 +16,7 @@ const useFetch = () => {
       if (response.ok === false) throw new Error(json.message);
     } catch (err) {
       json = null;
-      setError(err.mesage);
+      setError(err.message);
     } finally {
       setData(json);
       setLoading(false);
